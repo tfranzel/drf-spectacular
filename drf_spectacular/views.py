1016850dab17f0a4193186a2502861479f23dfdb
T. Franzel <13507857+tfranzel@users.noreply.github.com>
2020-04-25 18:50:17 +0200

    enable attr settings on SpectacularAPIView #35

--- drf_spectacular/views.py
+++ drf_spectacular/views.py
@@ -1,9 +1,11 @@
+from collections import namedtuple
+
 from rest_framework.renderers import JSONOpenAPIRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from drf_spectacular.settings import spectacular_settings
 from drf_spectacular.renderers import NoAliasOpenAPIRenderer
+from drf_spectacular.settings import spectacular_settings
 from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema
 
@@ -23,16 +25,19 @@ class SpectacularAPIView(APIView):
     renderer_classes = [NoAliasOpenAPIRenderer, JSONOpenAPIRenderer]
     permission_classes = spectacular_settings.SERVE_PERMISSIONS
 
+    generator_class = spectacular_settings.DEFAULT_GENERATOR_CLASS
+    serve_public = spectacular_settings.SERVE_PUBLIC
+    urlconf = spectacular_settings.SERVE_URLCONF
+
     @extend_schema(**SCHEMA_KWARGS)
     def get(self, request):
-        generator_class = spectacular_settings.DEFAULT_GENERATOR_CLASS
-        generator = generator_class(
-            urlconf=spectacular_settings.SERVE_URLCONF,
-        )
-        return Response(generator.get_schema(
-            request=request,
-            public=spectacular_settings.SERVE_PUBLIC
-        ))
+        if isinstance(self.urlconf, list):
+            ModuleWrapper = namedtuple('ModuleWrapper', ['urlpatterns'])
+            self.urlconf = ModuleWrapper(self.urlconf)
+
+        generator = self.generator_class(urlconf=self.urlconf)
+        schema = generator.get_schema(request=request, public=self.serve_public)
+        return Response(schema)
 
 
 class SpectacularYAMLAPIView(SpectacularAPIView):
--- tests/test_view.py
+++ tests/test_view.py
@@ -1,19 +1,59 @@
 import pytest
 import yaml
 from django.conf.urls import url
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework.test import APIClient
 
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema
 from drf_spectacular.validation import validate_schema
 from drf_spectacular.views import SpectacularAPIView
 
-urlpatterns = [url(r'^api/schema$', SpectacularAPIView.as_view(), name='schema')]
+
+@extend_schema(responses=OpenApiTypes.FLOAT)
+@api_view(http_method_names=['GET'])
+def pi(request):
+    return Response(3.1415)
+
+
+urlpatterns_v1 = [url(r'^api/v1/pi', pi)]
+urlpatterns_v1.append(
+    url(r'^api/v1/schema$', SpectacularAPIView.as_view(urlconf=urlpatterns_v1))
+)
+
+urlpatterns_v2 = [
+    url(r'^api/v2/pi', pi),
+    url(r'^api/v2/pi-fast', pi),
+]
+urlpatterns_v2.append(
+    url(r'^api/v2/schema$', SpectacularAPIView.as_view(urlconf=urlpatterns_v2)),
+)
+
+urlpatterns = urlpatterns_v1 + urlpatterns_v2
 
 
 @pytest.mark.urls(__name__)
 def test_spectacular_view(no_warnings):
-    response = APIClient().get('/api/schema')
+    response = APIClient().get('/api/v1/schema')
     assert response.status_code == 200
     assert response.content.startswith(b'openapi: 3.0.3\n')
     assert response.accepted_media_type == 'application/vnd.oai.openapi'
+
     schema = yaml.load(response.content, Loader=yaml.SafeLoader)
     validate_schema(schema)
+    assert len(schema['paths']) == 2
+
+
+@pytest.mark.urls(__name__)
+def test_spectacular_view_custom_urlconf(no_warnings):
+    response = APIClient().get('/api/v2/schema')
+    assert response.status_code == 200
+
+    schema = yaml.load(response.content, Loader=yaml.SafeLoader)
+    validate_schema(schema)
+    assert len(schema['paths']) == 3
+
+    response = APIClient().get('/api/v2/pi-fast')
+    assert response.status_code == 200
+    assert response.content == b'3.1415'
