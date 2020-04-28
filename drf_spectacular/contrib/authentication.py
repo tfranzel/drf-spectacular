2f48a47f2f804b29f2315296345ad3533e48ebcc
John Vandenberg <jayvdb@gmail.com>
2020-04-28 12:46:12 +0700

    Add djangorestframework-jwt auth handler
    Related to https://github.com/tfranzel/drf-spectacular/issues/28

--- drf_spectacular/contrib/authentication.py
+++ drf_spectacular/contrib/authentication.py
@@ -21,6 +21,20 @@ class SimpleJWTScheme(OpenApiAuthenticationExtension):
         }
 
 
+class JWTScheme(OpenApiAuthenticationExtension):
+    target_class = 'rest_framework_jwt.authentication.JSONWebTokenAuthentication'
+    name = 'jwtAuth'
+
+    def get_security_definition(self, auto_schema):
+        from rest_framework_jwt.settings import api_settings
+
+        return {
+            'type': 'http',
+            'scheme': 'bearer',
+            'bearerFormat': api_settings.JWT_AUTH_HEADER_PREFIX,
+        }
+
+
 class DjangoOAuthToolkitScheme(OpenApiAuthenticationExtension):
     target_class = 'oauth2_provider.contrib.rest_framework.OAuth2Authentication'
     name = 'oauth2'
