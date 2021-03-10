6b9159172ddf3e8057c7f130ea386bd02295c1e9
Matthias Erll <matthias.erll@credativ.de>
2021-03-10 15:18:26 +0100

    Fix map_renderers where format is None

--- drf_spectacular/openapi.py
+++ drf_spectacular/openapi.py
@@ -824,8 +824,9 @@ class AutoSchema(ViewInspector):
     def map_renderers(self, attribute):
         assert attribute in ['media_type', 'format']
         return list(dict.fromkeys([
-            getattr(r, attribute).split(';')[0] for r in self.view.get_renderers()
-            if not isinstance(r, renderers.BrowsableAPIRenderer)
+            getattr(r, attribute).split(';')[0]
+            for r in self.view.get_renderers()
+            if not isinstance(r, renderers.BrowsableAPIRenderer) and getattr(r, attribute)
         ]))
 
     def _get_serializer(self):
