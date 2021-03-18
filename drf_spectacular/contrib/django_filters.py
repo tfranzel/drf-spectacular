25772e60dbf0cf134f2143310338aee55a9b1a10
T. Franzel <tfranzel@users.noreply.github.com>
2021-03-18 13:41:09 +0100

    slighly improve #332 for django-filter range filters

--- drf_spectacular/contrib/django_filters.py
+++ drf_spectacular/contrib/django_filters.py
@@ -133,8 +133,13 @@ class DjangoFilterExtension(OpenApiFilterExtension):
             explode = True
             style = 'form'
         elif isinstance(filter_field, (filters.RangeFilter, filters.NumericRangeFilter)):
-            suffixes = filter_field.field_class.widget.suffixes
-            field_names = [f'{field_name}_{suffixes[0]}', f'{field_name}_{suffixes[1]}']
+            try:
+                suffixes = filter_field.field_class.widget.suffixes
+            except AttributeError:
+                suffixes = ['min', 'max']
+            field_names = [
+                f'{field_name}_{suffix}' if suffix else field_name for suffix in suffixes
+            ]
             explode = None
             style = None
         else:
