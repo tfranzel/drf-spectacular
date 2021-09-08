0e4e76f8679c2bb68ea1d31cc24dffdee21be998
Johannes Tamm <johannes.tamm@redfunction.ee>
2021-09-08 09:23:22 +0300

    fix: BinaryField's schema type should be binary #505

--- drf_spectacular/openapi.py
+++ drf_spectacular/openapi.py
@@ -435,8 +435,8 @@ class AutoSchema(ViewInspector):
         elif hasattr(models, 'JSONField') and isinstance(model_field, models.JSONField):
             # fix for DRF==3.11 with django>=3.1 as it is not yet represented in the field_mapping
             return build_basic_type(OpenApiTypes.OBJECT)
-        elif hasattr(models, 'BinaryField') and isinstance(model_field, models.BinaryField):
-            return build_basic_type(OpenApiTypes.BYTE)
+        elif isinstance(model_field, models.BinaryField):
+            return build_basic_type(OpenApiTypes.BINARY)
         elif hasattr(models, model_field.get_internal_type()):
             # be graceful when the model field is not explicitly mapped to a serializer
             internal_type = getattr(models, model_field.get_internal_type())
