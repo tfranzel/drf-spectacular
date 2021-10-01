3f7274348d265ef33c4eb8d0577dc83ba0ca36aa
Nick Pope <nick.pope@infogrid.io>
2021-10-01 13:54:18 +0100

    Tidy templates for documentation views.
    - Make spacing in `{{ ... }}` tags consistent.
    - No need for self-closing tags in HTML5.
    - Remove `type="text/css"` for `<link rel="stylesheet" ...>`
      - It's the implicit default since HTML5.
    - Don't force `type="image/png"` for Swagger favicon
      - The setting might point to another type, e.g. SVG.
    - Don't use the `|safe` filter unnecessarily, i.e. for `script_url`.
    - Various minor JavaScript tweaks, e.g. missing trailing semicolons, etc.

--- drf_spectacular/templates/drf_spectacular/swagger_ui.js
+++ drf_spectacular/templates/drf_spectacular/swagger_ui.js
@@ -1,22 +1,23 @@
-const swagger_settings  = {{settings|safe}}
+'use strict';
+{
+  const swagger_settings = {{ settings|safe }};
 
-const ui = SwaggerUIBundle({
-  url: "{{schema_url|safe}}",
-  dom_id: "#swagger-ui",
-  presets: [
-    SwaggerUIBundle.presets.apis,
-  ],
-  plugin: [
-    SwaggerUIBundle.plugins.DownloadUrl
-  ],
-  layout: "BaseLayout",
-  requestInterceptor: (request) => {
-    request.headers["X-CSRFToken"] = "{{csrf_token}}"
-    return request;
-  },
-  ...swagger_settings
-})
+  const ui = SwaggerUIBundle({
+    url: "{{ schema_url }}",
+    dom_id: "#swagger-ui",
+    presets: [
+      SwaggerUIBundle.presets.apis,
+    ],
+    plugin: [
+      SwaggerUIBundle.plugins.DownloadUrl,
+    ],
+    layout: "BaseLayout",
+    requestInterceptor: (request) => {
+      request.headers["X-CSRFToken"] = "{{ csrf_token }}";
+      return request;
+    },
+    ...swagger_settings,
+  })
 
-{% if oauth2_config %}
-ui.initOAuth({{oauth2_config|safe}})
-{% endif %}
+  {% if oauth2_config %}ui.initOAuth({{ oauth2_config|safe }});{% endif %}
+}
