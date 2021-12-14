8da7fece47c1062caeccebeb6708e699a3811baf
Roman Sichnyi <roman@sichnyi.com>
2021-12-14 03:59:07 -0500

    Fix schema generation for RecursiveField(many=True)

--- drf_spectacular/contrib/rest_framework_recursive.py
+++ drf_spectacular/contrib/rest_framework_recursive.py
@@ -1,9 +1,18 @@
+from rest_framework import serializers
+
 from drf_spectacular.extensions import OpenApiSerializerFieldExtension
+from drf_spectacular.plumbing import build_array_type
 
 
 class RecursiveFieldExtension(OpenApiSerializerFieldExtension):
     target_class = "rest_framework_recursive.fields.RecursiveField"
 
     def map_serializer_field(self, auto_schema, direction):
-        component = auto_schema.resolve_serializer(self.target.proxied, direction)
+        proxied = self.target.proxied
+
+        if isinstance(proxied, serializers.ListSerializer):
+            component = auto_schema.resolve_serializer(proxied.child, direction)
+            return build_array_type(component.ref)
+
+        component = auto_schema.resolve_serializer(proxied, direction)
         return component.ref
