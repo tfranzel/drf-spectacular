a2fdd8be1a734f25ef202a0364b88660726df079
T. Franzel <tfranzel@users.noreply.github.com>
2022-05-15 11:03:37 +0200

    minor fix and linting

--- drf_spectacular/settings.py
+++ drf_spectacular/settings.py
@@ -118,7 +118,7 @@ SPECTACULAR_DEFAULTS: Dict[str, Any] = {
     'GET_MOCK_REQUEST': 'drf_spectacular.plumbing.build_mock_request',
 
     # Camelize names like operationId and path parameter names
-    # Camelization of example JSON responses and Schema requires the addition of 
+    # Camelization of the operation schema itself requires the addition of
     # 'drf_spectacular.contrib.djangorestframework_camel_case.camelize_serializer_fields'
     # to POSTPROCESSING_HOOKS
     'CAMELIZE_NAMES': False,
