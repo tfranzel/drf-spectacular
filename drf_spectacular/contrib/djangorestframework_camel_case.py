b02b634df2b9ae84fd5ebfecdf1900921f98a828
T. Franzel <tfranzel@users.noreply.github.com>
2022-11-16 23:15:32 +0100

    bugfix djangorestframework_camel_case #861

--- drf_spectacular/contrib/djangorestframework_camel_case.py
+++ drf_spectacular/contrib/djangorestframework_camel_case.py
@@ -8,17 +8,21 @@ def camelize_serializer_fields(result, generator, request, public):
         return re.sub(camelize_re, underscore_to_camel, str)
 
     def camelize_component(schema: dict):
-        if 'properties' in schema:
-            schema['properties'] = {
-                camelize_str(field_name): camelize_component(field_schema)
-                for field_name, field_schema in schema['properties'].items()
-            }
-        if 'required' in schema:
-            schema['required'] = [camelize_str(field) for field in schema['required']]
+        if schema.get('type') == 'object':
+            if 'properties' in schema:
+                schema['properties'] = {
+                    camelize_str(field_name): camelize_component(field_schema)
+                    for field_name, field_schema in schema['properties'].items()
+                }
+            if 'required' in schema:
+                schema['required'] = [camelize_str(field) for field in schema['required']]
+        elif schema.get('type') == 'array':
+            camelize_component(schema['items'])
         return schema
 
-    for component in generator.registry._components.values():
-        camelize_component(component.schema)
+    for (_, component_type), component in generator.registry._components.items():
+        if component_type == 'schemas':
+            camelize_component(component.schema)
 
     # inplace modification of components also affect result dict, so regeneration is not necessary
     return result
--- tests/contrib/test_djangorestframework_camel_case.py
+++ tests/contrib/test_djangorestframework_camel_case.py
@@ -1,24 +1,33 @@
+import sys
+from typing import List
 from unittest import mock
 
 import pytest
 from rest_framework import mixins, serializers, viewsets
 from rest_framework.decorators import action
 
+# Use 3.9 instead of 3.8 version because of missing __required_keys__ feature,
+# which allows for a consistent test schema over different versions.
+if sys.version_info >= (3, 9):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
 from drf_spectacular.contrib.djangorestframework_camel_case import camelize_serializer_fields
-from drf_spectacular.drainage import TypedDict
 from drf_spectacular.utils import extend_schema
 from tests import assert_schema, generate_schema
 
 
 class NestedObject2(TypedDict):
-    field_six: int
-    field_seven: str
+    field_seven: int
+    field_eight: str
 
 
 class NestedObject(TypedDict):
     field_three: int
     field_four: str
-    field_five: 'NestedObject2'
+    field_five: NestedObject2
+    field_six: List[NestedObject2]
 
 
 class FakeSerializer(serializers.Serializer):
