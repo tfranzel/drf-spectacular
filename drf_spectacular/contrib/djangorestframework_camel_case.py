01b23b9f2472d6a2a53e738318aeb8cee80f85b9
T. Franzel <tfranzel@users.noreply.github.com>
2022-11-16 13:30:44 +0100

    bugfix djangorestframework_camel_case nested object handling #861

--- drf_spectacular/contrib/djangorestframework_camel_case.py
+++ drf_spectacular/contrib/djangorestframework_camel_case.py
@@ -2,15 +2,23 @@ import re
 
 
 def camelize_serializer_fields(result, generator, request, public):
-    from djangorestframework_camel_case.util import camelize, camelize_re, underscore_to_camel
+    from djangorestframework_camel_case.util import camelize_re, underscore_to_camel
+
+    def camelize_str(str):
+        return re.sub(camelize_re, underscore_to_camel, str)
+
+    def camelize_component(schema: dict):
+        if 'properties' in schema:
+            schema['properties'] = {
+                camelize_str(field_name): camelize_component(field_schema)
+                for field_name, field_schema in schema['properties'].items()
+            }
+        if 'required' in schema:
+            schema['required'] = [camelize_str(field) for field in schema['required']]
+        return schema
 
     for component in generator.registry._components.values():
-        if 'properties' in component.schema:
-            component.schema['properties'] = camelize(component.schema['properties'])
-        if 'required' in component.schema:
-            component.schema['required'] = [
-                re.sub(camelize_re, underscore_to_camel, key) for key in component.schema['required']
-            ]
+        camelize_component(component.schema)
 
     # inplace modification of components also affect result dict, so regeneration is not necessary
     return result
--- tests/contrib/test_djangorestframework_camel_case.py
+++ tests/contrib/test_djangorestframework_camel_case.py
@@ -5,13 +5,30 @@ from rest_framework import mixins, serializers, viewsets
 from rest_framework.decorators import action
 
 from drf_spectacular.contrib.djangorestframework_camel_case import camelize_serializer_fields
+from drf_spectacular.drainage import TypedDict
 from drf_spectacular.utils import extend_schema
 from tests import assert_schema, generate_schema
+
+
+class NestedObject2(TypedDict):
+    field_six: int
+    field_seven: str
+
+
+class NestedObject(TypedDict):
+    field_three: int
+    field_four: str
+    field_five: 'NestedObject2'
 
 
 class FakeSerializer(serializers.Serializer):
     field_one = serializers.CharField()
     field_two = serializers.CharField()
+
+    field_nested = serializers.SerializerMethodField()
+
+    def get_field_nested(self) -> NestedObject:  # type: ignore
+        pass  # pragma: no cover
 
 
 class FakeViewset(mixins.ListModelMixin, viewsets.GenericViewSet):
