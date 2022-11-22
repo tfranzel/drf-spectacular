c7209e06425fff38b922a9971a484ea616649f37
Numerlor <numerlor@numerlor.me>
2022-11-22 21:16:20 +0100

    Use direct view methods for getting serializer instances
    The methods that get instances can be customized by users, so going around them and using the type directly could go around the user customization.

--- docs/blueprints/drf_rw_serializers.py
+++ docs/blueprints/drf_rw_serializers.py
@@ -8,10 +8,10 @@ class CustomAutoSchema(AutoSchema):
 
     def get_request_serializer(self):
         if isinstance(self.view, RWGenericAPIView):
-            return self.view.get_write_serializer_class()()
+            return self.view.get_write_serializer()
         return self._get_serializer()
 
     def get_response_serializers(self):
         if isinstance(self.view, RWGenericAPIView):
-            return self.view.get_read_serializer_class()()
+            return self.view.get_read_serializer()
         return self._get_serializer()
