fd5380bce822e90fd30d9f5e9be1dff82c520370
Đào Minh Hạt <enjoy3013@gmail.com>
2023-03-07 14:05:14 +0700

    Add PresentablePrimarKeyRelatedField schema for drf-exrta-fields blueprint
    Add PresentablePrimarKeyRelatedField schema for drf-exrta-fields blueprint

--- docs/blueprints/drf_extra_fields.py
+++ docs/blueprints/drf_extra_fields.py
@@ -1,6 +1,9 @@
 from drf_spectacular.extensions import OpenApiSerializerFieldExtension
+from drf_spectacular.openapi import AutoSchema
+from drf_spectacular.plumbing import append_meta
 from drf_spectacular.plumbing import build_basic_type
 from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import Direction
 
 
 class Base64FileFieldSchema(OpenApiSerializerFieldExtension):
@@ -18,3 +21,20 @@ class Base64FileFieldSchema(OpenApiSerializerFieldExtension):
 
 class Base64ImageFieldSchema(Base64FileFieldSchema):
     target_class = "drf_extra_fields.fields.Base64ImageField"
+
+
+class PresentablePrimaryKeyRelatedFieldSchema(OpenApiSerializerFieldExtension):
+    target_class = 'drf_extra_fields.relations.PresentablePrimaryKeyRelatedField'
+
+    def map_serializer_field(self, auto_schema: AutoSchema, direction: Direction):
+        if direction == 'request':
+            return build_basic_type(OpenApiTypes.INT)
+
+        meta = auto_schema._get_serializer_field_meta(self.target, direction)
+        schema = auto_schema.resolve_serializer(
+            self.target.presentation_serializer(
+                context=self.target.context, **self.target.presentation_serializer_kwargs,
+            ),
+            direction,
+        ).ref
+        return append_meta(schema, meta)
